606c09f5dc56cc6fea257d11e60038547b1774a8
willeff <jchen0827@hotmail.com>
2023-09-28 17:42:26 -0400

    Change added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { logo } from '../utils/constants';
-
+import SearchBar from './SearchBar';
 const Navbar = () => (
 <Stack 
   direction = 'row' 
@@ -12,6 +12,7 @@ const Navbar = () => (
   <Link to="/" style={{display: 'flex', alignItems: 'center'}}>
     <img src = {logo} alt = 'logo' height = {50} />
   </Link>
+  <SearchBar/>
 </Stack>
 )
 
