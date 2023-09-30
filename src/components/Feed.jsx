92d18562f2d3915a3e9393b1d882811cdb07eeb0
willeff <jchen0827@hotmail.com>
2023-09-29 23:20:59 -0400

    Feed and sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { useState, useEffect} from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { Sidebar, Videos } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+  },[selectedCategory]);
+
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row"} }}>
+      <Box sx={{ height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d', px: { sx: 0, md: 2 }}}>
+        <Sidebar 
+          selectedCategory = {selectedCategory}
+          setSelectedCategory = {setSelectedCategory}
+        />
+
+        <Typography className="copyright" variant='body5' sx={{ mt: 1.5, color: 'teal'}}>
+          Copyright 2023 J.C
+
+
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{color:'white'}}>
+          {selectedCategory} <span style={{color:'teal'}}>videos</span>
+        </Typography>
+
+        <Videos videos={videos}/>
+      </Box>
+    </Stack>
   )
 }
 
